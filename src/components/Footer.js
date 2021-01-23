6f35e9b3408753672ae2f0518fdce271c0133ffa
Katie L <katie_acting@live.ca>
2021-01-23 02:01:40 -0500

    Add dynamic year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Footer = (props) => (
-    <footer style={{background: props.background}} className="pt-5 pb-2 w-100">
-        <div className="container mt-4" style={{height: "100px"}}>
+    <footer style={{ background: props.background }} className="pt-5 pb-2 w-100">
+        <div className="container mt-4" style={{ height: "100px" }}>
         </div>
-        <p className="w-100 block text-right text-light pr-5">Copyright &copy; TechBuy 2018</p>
+        <p className="w-100 block text-right text-light pr-5">Copyright &copy; TechBuy {new Date().getFullYear()}</p>
     </footer>
 )
 
